732bb3b5d3ca501517cf5070ce15ed86ea8d0124
Roberto Vicario <rvicariodev@gmail.com>
2024-07-01 00:11:05 +0200

    updates

--- CNN/CNN.py
+++ CNN/CNN.py
@@ -9,11 +9,10 @@ import matplotlib.pyplot as plt
 X_train, X_test = X_train / 255.0, X_test / 255.0
 
 num_classes = 10
+input_shape = X_train.shape[1:]
 
 y_train = to_categorical(y_train, num_classes)
 y_test = to_categorical(y_test, num_classes)
-
-input_shape = X_train.shape[1:]
 
 model = Sequential()
 
